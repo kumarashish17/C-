46a4ec75d04aef622aebbf4d6e3fa3a4350119db
Ashish <ashishkk948@gmail.com>
2025-09-16 07:40:15 +0530

    code updated

---
+++ array/min.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Size";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int min= INT_MAX;
+    for(int i=0;i<n;i++){
+    if(min>arr[i]) min=arr[i];
+    }
+    cout<<min;
+}
---
+++ array/secondMax.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Size";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int max= INT_MIN;
+    for(int i=0;i<n;i++){
+    if(max<arr[i]) max=arr[i];
+    }
+    cout<<max<<endl;
+    int smax=INT_MIN;
+    for(int i=0;i<n;i++){
+    if(smax<arr[i] && arr[i]!=max) smax=arr[i];
+    }
+    cout<<smax;
+    }
