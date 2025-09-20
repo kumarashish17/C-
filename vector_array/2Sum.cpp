e71ffd5e638d8606d5838c68b19c02ab3c5cfbcc
Ashish <ashishkk948@gmail.com>
2025-09-20 09:30:47 +0530

    updated

--- vector_array/2Sum.cpp
+++ vector_array/2Sum.cpp
@@ -14,7 +14,7 @@ int main(){
     int target;
     cout<<"Enter Target";   
     cin>>target;
-    for(int i=0;i<v.size();i++){
+    for(int i=0;i<v.size()-1;i++){
         for(int j=i+1;j<v.size();j++){
             if(v[i]+v[j]==target){
                 cout<<i<<" "<<j;
